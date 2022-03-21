38a39967c9cb80632d98e7eee038e1978ec35083
Rik Jansen <r.p.m.jansen@gmail.com>
2022-03-21 14:53:04 +0100

    Entity framework model opgezet

---
+++ User Service/User Service/DAL Layer/Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL_Layer
+{
+    public class UserContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"connection string, moet via appsettings file");
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
